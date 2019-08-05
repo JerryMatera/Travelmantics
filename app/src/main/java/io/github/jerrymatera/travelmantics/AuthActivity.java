e6a4ab3f2c194989b76d60a6b67e5af4222545ec
JerryMatera <jerryonyango.95@gmail.com>
2019-08-05 17:01:11 +0300

    Initial commit

---
+++ app/src/main/java/io/github/jerrymatera/travelmantics/AuthActivity.java
@@ -0,0 +1,14 @@
+package io.github.jerrymatera.travelmantics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
